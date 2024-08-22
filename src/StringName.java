e663106125077c7214f39d62814b5eef52acb2da
dlin <david.lin@smes.org>
2024-08-22 09:56:21 -0700

    Class 1

---
+++ src/StringName.java
@@ -0,0 +1,41 @@
+//this is a code comment
+//notes or explanations for what the code does
+//doesn't get run as part of the program
+
+/*
+this is a block comment
+-don't need // within a block comment
+
+Java classes are program files
+-class names need to match the file name
+-class names need to start with a Capital letter
+and have no spaces
+-all code will go in the curly braces for the class
+ */
+
+public class StringName {
+
+    //this is the main method
+    //for now, all code we write goes in the main method
+    public static void main(String [] args) {
+
+        //this is a print statement - same as console.log in JS
+        System.out.println("hello world");
+
+        //"hello world" is in quotes - it's a string
+        //Strings are chars/nums/text that can be output or
+        //or used as data in the program
+        System.out.print("hi");
+
+        //-System.out.print() (NOT prinln()) will output
+        //immediately after the previous output on the same line
+        //-System.out.println() inserts a line break AFTER what gets output
+        System.out.print("hi");
+
+        System.out.println("something else");
+
+        System.out.println(); //empty println() to give an extra line break
+        System.out.println("blah");
+
+    }
+}
